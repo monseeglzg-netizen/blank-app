526ea7e32292728b9598af489f32572702f3e5d1
monseeglzg-netizen <monseeglzg@gmail.com>
2025-12-04 09:25:14 -0600

    Initialize Streamlit app with page configuration
    Added imports and initial configuration for the Streamlit app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,10 @@
+import altair as alt
+import pandas as pd
+import streamlit as st
 
+# Configuración de la página
+st.set_page_config(page_title="Temperatura Mexico", page_icon="🎬")
+st.title("Temperatura Mexico")
+st.write("""
+En esta página podrás predecir las temperaturas de las ciudades de Mexico.
+""")
